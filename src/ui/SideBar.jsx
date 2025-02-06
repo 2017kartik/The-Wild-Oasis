9ebf37c7b12d3100ac157a6f76795126c4b4ba83
2017kartik <kartikagarwal9119063420@gmail.com>
2025-02-06 08:35:26 +0530

    Fix styling bugs

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -6,7 +6,7 @@ import Uploader from "../data/Uploader";
 const StyleSideBar = styled.aside`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
-  border-right: 1px solid var(--color-grey-500);
+  border-right: 1px solid var(--color-grey-100);
   grid-row: 1 / -1;
   display: flex;
   flex-direction: column;

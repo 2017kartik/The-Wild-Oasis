542c263436fc62e23e9fbd9ec0f32fcea3ba3475
2017kartik <kartikagarwal9119063420@gmail.com>
2025-02-09 18:17:19 +0530

    Displaying Stays for Current Day

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useCabins } from "../cabins/useCabins";
 import Spinner from "../../ui/Spinner";
 import Stats from "./Stats";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -36,7 +37,7 @@ export default function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Today&apos;s activity</div>
-      <div>Chart stay durations</div>
+      <DurationChart confirmStays={confirmedStays} />
       <div>Chart sale</div>
 
       <SalesChart bookings={bookings} numDays={numDays} />

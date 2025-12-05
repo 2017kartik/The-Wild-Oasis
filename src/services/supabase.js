a94fd614d5eec5d534468f43ba253a0349d4e045
2017kartik <kartikagarwal9119063420@gmail.com>
2025-12-05 13:52:20 +0530

    Make changes on cabin images

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://lediiuxvvbwazyygglih.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlZGlpdXh2dmJ3YXp5eWdnbGloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5MTUxMTksImV4cCI6MjA4MDQ5MTExOX0.5Z9kubozf19HWd6keEvAhqTnsqCOUoh8sRYiZ-JdOaQ";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
